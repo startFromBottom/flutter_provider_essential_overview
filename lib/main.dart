f38c01020cae16cbd48b9ff28c03b23811960a3f
hyuem <uhh0701@gmail.com>
2022-05-25 22:51:51 +0900

    Chapter 15 - Consumer

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<Dog>(
-      create: (context) => Dog(name: 'dog05', breed: 'breed05', age: 3),
+      create: (context) => Dog(name: 'dog08', breed: 'breed08', age: 3),
       child: MaterialApp(
-        title: 'Provider 05',
+        title: 'Provider 08',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -39,20 +39,30 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Provider 05'),
+        title: Text('Provider 08'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '- name: ${context.watch<Dog>().name}',
-              style: TextStyle(fontSize: 20.0),
+      body: Consumer<Dog>(
+        builder: (context, Dog dog, Widget? child) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                child!,
+                SizedBox(height: 10.0),
+                Text(
+                  '- name: ${dog.name}',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                SizedBox(height: 10.0),
+                BreedAndAge(),
+              ],
             ),
-            SizedBox(height: 10.0),
-            BreedAndAge(),
-          ],
+          );
+        },
+        child: Text(
+          "I like dogs very much",
+          style: TextStyle(fontSize: 20.0),
         ),
       ),
     );
@@ -66,16 +76,18 @@ class BreedAndAge extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          '- breed: ${context.select<Dog, String>((Dog dog) => dog.breed)}',
-          style: TextStyle(fontSize: 20.0),
-        ),
-        SizedBox(height: 10.0),
-        Age(),
-      ],
-    );
+    return Consumer<Dog>(builder: (_, Dog dog, __) {
+      return Column(
+        children: [
+          Text(
+            '- breed: ${dog.breed}',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          SizedBox(height: 10.0),
+          Age(),
+        ],
+      );
+    });
   }
 }
 
@@ -86,21 +98,23 @@ class Age extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          '- age: ${context.select<Dog, int>((Dog dog) => dog.age)}',
-          style: TextStyle(fontSize: 20.0),
-        ),
-        SizedBox(height: 20.0),
-        ElevatedButton(
-          onPressed: () => context.read<Dog>().grow(),
-          child: Text(
-            'Grow',
+    return Consumer<Dog>(builder: (_, dog, __) {
+      return Column(
+        children: [
+          Text(
+            '- age: ${dog.age}',
             style: TextStyle(fontSize: 20.0),
           ),
-        ),
-      ],
-    );
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: () => dog.grow(),
+            child: Text(
+              'Grow',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
